bittencourtjulival/eclipse_kernel_xiaomi_stone

net: Rename FLOWI_FLAG_VRFSRC to FLOWI_FLAG_L3MDEV_SRC
Signed-off-by: David Ahern <dsa@cumulusnetworks.com>
Signed-off-by: David S. Miller <davem@davemloft.net>

include/net/flow.h
@@ -34,7 +34,7 @@ struct flowi_common {
 	__u8	flowic_flags;
 #define FLOWI_FLAG_ANYSRC		0x01
 #define FLOWI_FLAG_KNOWN_NH		0x02
-#define FLOWI_FLAG_VRFSRC		0x04
+#define FLOWI_FLAG_L3MDEV_SRC		0x04
 #define FLOWI_FLAG_SKIP_NH_OIF		0x08
 	__u32	flowic_secid;
 	struct flowi_tunnel flowic_tun_key;

eladraz/vrf_vackport_linux-3.10.0-327.el7

include/net/flow.h
@@ -23,7 +23,7 @@ struct flowi_common {
 /* This has changed to 0x02 upstream. Preserved here to remain ABI compatible. */
 #define FLOWI_FLAG_KNOWN_NH		0x04
 /* This was to 0x04 upstream. Changed here to 0x05. */
-#define FLOWI_FLAG_VRFSRC		0x05
+#define FLOWI_FLAG_L3MDEV_SRC		0x05
 #define FLOWI_FLAG_SKIP_NH_OIF		0x08
 	__u32	flowic_secid;
 };